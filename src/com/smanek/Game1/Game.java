fa6dffbe16f7f76ccb02bd2a5bbb2d564b05d778
Sameer Manek <sameermanek@hotmail.com>
2018-12-02 12:28:30 +0530

    added buffer rendering and image raster

--- src/com/smanek/Game1/Game.java
+++ src/com/smanek/Game1/Game.java
@@ -1,7 +1,12 @@
 package com.smanek.Game1;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
@@ -17,6 +22,9 @@ public class Game extends Canvas implements Runnable {
 	private JFrame frame;
 	private boolean running = false;
 	
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB); // created an image to be rendered
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData(); // converting the image to raster and fetching each pixel in integer form (enables access to the image being rendered).
+	
 	public Game()
 	{
 		Dimension size = new Dimension(WIDTH*scale, HEIGHT*scale);
@@ -31,12 +39,34 @@ public class Game extends Canvas implements Runnable {
 		running = true;
 		thread.start();
 	}
+	
+	public void update()
+	{}
+	
+	public void render()
+	{
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null) 
+		{
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		g.dispose(); // disposes buffer after calculation
+		bs.show(); // render the buffer that has been calculated
+	}
 
 	@Override
 	public void run() {
 		while(running)
 		{
-			System.out.println("this is running..");
+			update();
+			render();
 		}
 	}
 	
